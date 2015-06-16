9bcad12d9d887afaa1e077609ba23ca115c67f23
Tomasz Kuczma <tomaszkuczma@o2.pl>
2015-06-16 15:30:42 +0200

    colors inited

--- xcowsay.c
+++ xcowsay.c
@@ -8,18 +8,41 @@
 
 #define COMMAND "fortune -a | fmt -80 -s | $(shuf -n 1 -e cowsay cowthink) -$(shuf -n 1 -e b d g p s t w y) -f $(shuf -n 1 -e $(cowsay -l | tail -n +2)) -n"
 #define SLEEP_IN_SEC 15
+#define PIXELS_SIZE 6
+#define STRIP_LEN 4
+#define BUF_SIZE 1024
 
-void draw(Display *dpy, Window root, XWindowAttributes wa, GC g, const int lineHeight) {
+void draw(Display *dpy, Window root, XWindowAttributes wa, GC g, XFontStruct* fs) {
 	int posX = random()%(wa.width/2), posY = random()%(wa.height/2) + 10;
 	FILE *pp = popen(COMMAND, "r");
 	if (!pp) return;
 
+	//red, orange, yellow, green, blue, violet
+	unsigned long pixels[PIXELS_SIZE]= {0xFF0000, 0xFFA500, 0xFFFF00, 0x00A500, 0x0000A5, 0xA500A5};
+	/* get line height */
+	int lineHeight = fs ? fs->ascent+fs->descent : 13;
+
 	while(1) {
-		char buf[1000];
+		char buf[BUF_SIZE];
 		char* line = fgets(buf, sizeof(buf), pp);
-		
+
 		if (!line) break;
-		XDrawString(dpy, root, g, posX, (posY+=lineHeight), line, strlen(line)-1);
+
+		int i=0, len = strlen(line)-1;
+		/* set up end of string */
+		for(i=len; i<len+STRIP_LEN && i<sizeof(buf); i++) 
+			line[i] = ' ';
+
+		int posXTmp = posX;
+
+		for(i=0; i<len; i+=STRIP_LEN) {
+			XSetForeground(dpy, g, pixels[(i/STRIP_LEN)%PIXELS_SIZE]);
+
+			XDrawString(dpy, root, g, posXTmp, posY, line+i, STRIP_LEN);
+			posXTmp += XTextWidth(fs, line+i, STRIP_LEN);
+		}
+
+		posY += lineHeight;
 	}
 
 	pclose(pp);
@@ -31,8 +54,6 @@ int main()
 	Window root;
 	XWindowAttributes wa;
 	GC g;
-
-	XColor redx, reds;
 
 	Font f;
 	XFontStruct *fs;
@@ -50,13 +71,6 @@ int main()
 	/* create a GC for drawing in the window */
 	g = XCreateGC(dpy, root, 0, NULL);
 
-	/* allocate the red color */
-	XAllocNamedColor(dpy, DefaultColormapOfScreen(DefaultScreenOfDisplay(dpy)),
-		"red", &reds, &redx);
-
-	/* set foreground color */
-	XSetForeground(dpy, g, reds.pixel);
-
 	/* load a font */
 	f=XLoadFont(dpy, "fixed");
 	XSetFont(dpy, g, f);
@@ -65,15 +79,12 @@ int main()
 	XGetGCValues (dpy, g, GCFont, &v);
 	fs = XQueryFont (dpy, v.font);
 
-	/* get line height */
-	int lineHeight = fs ? fs->ascent+fs->descent : 13;
-
 	/* draw something */
 	while(1)
 	{
 		XClearWindow(dpy, root);
 
-		draw(dpy, root, wa, g, lineHeight);
+		draw(dpy, root, wa, g, fs);
 
 		/* flush changes and sleep */
 		XFlush(dpy);
